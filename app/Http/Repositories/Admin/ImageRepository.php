a453678defb44b5156d96fd7f2259f88c9886d6f
ziyadragab <ziyadragab00@gmail.com>
2023-11-24 21:24:05 +0200

    Add Image Section

---
+++ app/Http/Repositories/Admin/ImageRepository.php
@@ -0,0 +1,69 @@
+<?php
+namespace App\Http\Repositories\Admin;
+
+
+use App\Http\Interfaces\Admin\ImageInterface;
+use App\Http\Traits\ImagesTrait;
+use App\Models\Image;
+
+class ImageRepository implements ImageInterface{
+
+    use ImagesTrait;
+
+    private $imageMode;
+    public function __construct(Image $image)
+    {
+     $this->imageMode=$image;
+    }
+    public function index($dataTable)
+    {
+       return $dataTable->render('admin.pages.image.index');
+    }
+    public function create()
+    {
+       return view('admin.pages.image.create');
+    }
+    public function store($request)
+    {
+
+        $imageName = $this->uploadImage($request->file('image'), $this->imageMode::PATH);
+
+        $this->imageMode::create([
+            'image' => $imageName,
+        ]);
+
+        toast('Image Created Successfully', 'success');
+        return redirect(route('admin.image.index'));
+
+    }
+    public function edit($image)
+    {
+      return view('admin.pages.image.edit',compact('image'));
+    }
+    public function update($image, $request)
+    {
+      if($request->image){
+       $newImage= $this->uploadImage($request->image,$this->imageMode::PATH , $image->image);
+      }
+      $image->update([
+      'image'=>$newImage ??$image->getRawOriginal('image')
+      ]);
+      toast('Image Updated Successfully','success');
+      return redirect(route('admin.image.index'));
+    }
+    public function delete($image)
+    {
+
+      $this->removeImage($image->image);
+      $image->delete();
+      toast('Image Deleted Successfully','success');
+      return redirect(route('admin.image.index'));
+    }
+}
+
+
+
+
+
+
+?>
